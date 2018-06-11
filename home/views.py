66cc42ab74f69f16e515131771203fc5d240b79d
Miguel Martinez <miguelitalia@hotmail.com>
2018-06-11 15:43:45 +0000

    showing the different authors with the respective books

--- home/views.py
+++ home/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Author
 
 
 # Create your views here.
 def get_index(request):
-    book_items = Book.objects.all()   # this gets all from database sqlite3
-    return render(request, "home/index.html", {"books": book_items})
+    authors = Author.objects.all()   # this gets all from database sqlite3
+    return render(request, "home/index.html", {"authors": authors})
