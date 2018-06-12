109806cf67416a70bc55532d7fd710cd50ff0bbd
Miguel Martinez <miguelitalia@hotmail.com>
2018-06-12 13:16:54 +0000

    slightly styled page

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib import auth
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def login(request):
-    return render(request, "accounts/login.html")
+    if request.method == "POST":
+        #gets the username and password off the form
+        u = request.POST["username"]
+        p = request.POST["password"]
+        #gives the username and password to the variable user
+        user = authenticate(username = u, password = p)
+        
+        if user is not None:
+            auth.login(request, user) # we use auth as we are importing from line 3 and if we don't use auth.login it will conflict like with the logout function underneath
+            return redirect("/")
+        else:
+            return HttpResponse("That user or password is wrong!")
+    else:
+        return render(request, "accounts/login.html")
     
 def register(request):
     return render(request, "accounts/register.html")
     
 def logout(request):
-    return render(request, "home/index.html")
+    auth.logout(request)
+    return redirect('/') 
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from home.views import get_index
-from accounts.views import login, register
+from accounts.views import login, register, logout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', get_index),
     url(r'^accounts/login$', login),
-    url(r'^accounts/register$', register)
+    url(r'^accounts/register$', register),
+    url(r'^accounts/logout$', logout)
 ]
